95e32f434cc33a66be940e13b9ca3a05beff1bf2
gaelestrada <gaelestrada1979@gmail.com>
2026-03-27 10:50:09 -0300

    Subo actividad 5

---
+++ Gaelestrada-act5/Punto1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Punto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Confeccionar un programa que permita ingresar un valor del 1 al 10
+            // y mostrar la tabla de multiplicar del mismo (primeros 13 términos).
+
+            int numero;
+            string linea;
+
+            Console.Write("Ingrese un numero del 1 al 10: ");
+            linea = Console.ReadLine();
+            numero = int.Parse(linea);
+
+            for (int i = 1; i <= 13; i++)
+            {
+                Console.WriteLine(numero * i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
